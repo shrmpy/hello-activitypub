1b01371f6fb3370b78e209a53cff2249dd4f698e
shrmpy <>
2022-11-22 00:13:22 -0800

    netlify and beyond

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'clum60/app';
+import config from 'clum60/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
