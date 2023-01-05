968ba36a23f899738bad03704ec2c2929e41c3a0
shrmpy <>
2023-01-05 00:31:01 -0800

    shift to pre-build

--- plugins/netlify-plugin-activitystub/index.js
+++ plugins/netlify-plugin-activitystub/index.js
@@ -1,21 +1,24 @@
 import { stubRedirects, stubSkeleton, stubHeaders } from './formatters.js'
 import { stubActor } from './keypair.js'
 
-export const onPreBuild = async function({ netlifyConfig }) {
+export const onPreBuild = async function({ constants, netlifyConfig, utils: { run }}) {
     netlifyConfig.redirects.push(...stubRedirects())
     stubSkeleton()
+    // define actor
     const tmprd = await stubActor()
     netlifyConfig.redirects.push(...tmprd)
+    // define headers
     netlifyConfig.headers.push(...stubHeaders())
+    // make functions
+    netlifyConfig.build.environment.GOBIN = constants.FUNCTIONS_SRC
+    await run.command('mkdir -p ' + constants.FUNCTIONS_SRC)
+    await run.command('go install ./...')
 }
 
 export const onBuild = async function({ constants, netlifyConfig, utils: { functions, run }}) {
     //TODO can we package function src files and copy into the build base dir?
     //     it's not clear, so just call go-install and assume files exist (cmd/*)
-    netlifyConfig.build.environment.GOBIN = constants.FUNCTIONS_SRC
-    await run.command('mkdir -p ' + constants.FUNCTIONS_SRC)
-    await run.command('go install ./...')
-    await functions.add(constants.FUNCTIONS_SRC)
+    ////await functions.add(constants.FUNCTIONS_SRC)
 }
 
 
