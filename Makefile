094f5cbc57d8c06bd17155624eaa2af1607c73e2
Shrimp Research <shrmpy@users.noreply.github.com>
2023-01-05 09:09:11 +0000

    Makefile (#3)
    * stubHeaders
    
    * onBuild attempt
    
    * for now
    
    * GOBIN
    
    * netlify/functions
    
    * functions.add
    
    * shift to pre-build
    
    * go verbose
    
    * go build
    
    * go build
    
    * revisit prebuild
    
    Co-authored-by: shrmpy <>

--- Makefile
+++
@@ -1,6 +0,0 @@
-
-build:
-	mkdir -p netlify/functions
-	GOBIN=${PWD}/netlify/functions go install ./...
-	astro build
-
--- plugins/netlify-plugin-activitystub/index.js
+++ plugins/netlify-plugin-activitystub/index.js
@@ -1,30 +1,25 @@
-import { stubRedirects, stubSkeleton } from './formatters.js'
+import { stubRedirects, stubSkeleton, stubHeaders } from './formatters.js'
 import { stubActor } from './keypair.js'
 
 export const onPreBuild = async function({ netlifyConfig }) {
-
     netlifyConfig.redirects.push(...stubRedirects())
     stubSkeleton()
+    // define actor
     const tmprd = await stubActor()
     netlifyConfig.redirects.push(...tmprd)
+    // define headers
+    netlifyConfig.headers.push(...stubHeaders())
+}
 
-    // Add headers
-    netlifyConfig.headers.push({
-        for: '/u/*',
-        values: { 'Content-Type': "application/activity+json" },
-      },
-      {
-        for: '/l/*',
-        values: { 'Content-Type': "application/octet-stream" },
-      },
-      {
-        for: '/.well-known/webfinger',
-        values: {
-          'Content-Type': "application/jrd+json",
-          'Access-Control-Allow-Origin': "*",
-      },
-    })
-
+export const onBuild = async function({ constants, utils: { functions, run }}) {
+    //TODO can we package function src files and copy into the build base dir?
+    //     it's not clear, so just call go-install and assume files exist (cmd/*)
+    ////await functions.add(constants.FUNCTIONS_SRC)
+    ////netlifyConfig.build.environment.GOBIN = constants.FUNCTIONS_SRC
+    await run.command('mkdir -p ' + constants.FUNCTIONS_SRC)
+    ////await run.command('go install -v -x -a ./...')
+    await run.command('go build -o ' + constants.FUNCTIONS_SRC + '/inbox cmd/inbox/main.go')
+    await run.command('go build -o ' + constants.FUNCTIONS_SRC + '/sort cmd/sort/main.go')
 }
 
 
