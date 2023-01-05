ab3a35b54bc071e6f5134338e8c5324d90fd059a
shrmpy <>
2023-01-05 01:02:02 -0800

    revisit prebuild

--- plugins/netlify-plugin-activitystub/index.js
+++ plugins/netlify-plugin-activitystub/index.js
@@ -1,7 +1,7 @@
 import { stubRedirects, stubSkeleton, stubHeaders } from './formatters.js'
 import { stubActor } from './keypair.js'
 
-export const onPreBuild = async function({ constants, netlifyConfig, utils: { run }}) {
+export const onPreBuild = async function({ netlifyConfig }) {
     netlifyConfig.redirects.push(...stubRedirects())
     stubSkeleton()
     // define actor
@@ -9,18 +9,17 @@ export const onPreBuild = async function({ constants, netlifyConfig, utils: { ru
     netlifyConfig.redirects.push(...tmprd)
     // define headers
     netlifyConfig.headers.push(...stubHeaders())
-    // make functions
-    netlifyConfig.build.environment.GOBIN = constants.FUNCTIONS_SRC
+}
+
+export const onBuild = async function({ constants, utils: { functions, run }}) {
+    //TODO can we package function src files and copy into the build base dir?
+    //     it's not clear, so just call go-install and assume files exist (cmd/*)
+    ////await functions.add(constants.FUNCTIONS_SRC)
+    ////netlifyConfig.build.environment.GOBIN = constants.FUNCTIONS_SRC
     await run.command('mkdir -p ' + constants.FUNCTIONS_SRC)
     ////await run.command('go install -v -x -a ./...')
     await run.command('go build -o ' + constants.FUNCTIONS_SRC + '/inbox cmd/inbox/main.go')
     await run.command('go build -o ' + constants.FUNCTIONS_SRC + '/sort cmd/sort/main.go')
-}
-
-export const onBuild = async function({ constants, netlifyConfig, utils: { functions, run }}) {
-    //TODO can we package function src files and copy into the build base dir?
-    //     it's not clear, so just call go-install and assume files exist (cmd/*)
-    ////await functions.add(constants.FUNCTIONS_SRC)
 }
 
 
